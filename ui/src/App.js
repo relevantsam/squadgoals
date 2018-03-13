6a33127884f7d8131a37f22d0d2e9ef954637c37
Sam Bartlett <sab@auburn.edu>
2018-03-13 00:36:04 -0500

    update dependencies, start collecting shard data about user in ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/pubg_logo.png';
+import '../node_modules/bulma/css/bulma.css';
 import './App.css';
+
+import Onboarding from './onboarding';
 
 class App extends Component {
   render() {
@@ -8,11 +11,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="title">#SQUADGOALS</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Onboarding />
       </div>
     );
   }
