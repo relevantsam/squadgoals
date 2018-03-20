732826c1078078cc2d7817dbfce576c8f78afddf
Sam <sam@sasbartlett.com>
2018-03-19 21:53:35 -0500

    add ability to filter matches by single player (optimize front query)

--- squadgoals-api/src/api/matches/index.js
+++ squadgoals-api/src/api/matches/index.js
@@ -7,8 +7,10 @@ const router = express.Router();
 router.get('/', (req, res) => {
     const shard = req.query.shard || "pc-na";
     const api = new PUBGAPI();
+    let filters = {};
+    if(req.query.player) filters = { playerNames: req.query.player};
 
-    api.getMatches(shard, {}).then((response) => {
+    api.getMatches(shard, filters).then((response) => {
         res.status(200).json(response);
     }).catch((err) => {
         console.log(err);
