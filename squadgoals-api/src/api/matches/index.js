eb697f2136a93514c3882c619c9e013b49b82fa1
Sam Bartlett <sab@auburn.edu>
2018-03-25 09:59:21 -0500

    add match individual endpoint

--- squadgoals-api/src/api/matches/index.js
+++ squadgoals-api/src/api/matches/index.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
     const shard = req.query.shard || "pc-na";
     const api = new PUBGAPI(process.env.PUBG_TOKEN);
     let filters = {};
-    if(req.query.player) filters = { playerNames: req.query.player};
+    if(req.query.playerNames) filters = { playerNames: req.query.player};
 
     api.getMatches(shard, filters).then((response) => {
         res.status(200).json(response);
@@ -17,6 +17,18 @@ router.get('/', (req, res) => {
         res.status(err.response.status).json({message: err.response.data});
     })
 });
+
+router.get('/:matchId', (req, res) => {
+    const shard = req.query.shard || "pc-na";
+    const api = new PUBGAPI(process.env.PUBG_TOKEN);
+    
+    api.getMatch(shard, req.params.matchId).then((response) => {
+        res.status(200).json(response);
+    }).catch((err) => {
+        console.log(err);
+        res.status(err.response.status).json({message: err.response.data});
+    })
+})
 
 router.use('/squads', SquadsRouter)
 
